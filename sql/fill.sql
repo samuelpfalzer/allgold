4e4d45f6c2c315cb631776a88754719a1e7b0a1a
Samuel Pfalzer <samuel.pfalzer@stud.hs-kempten.de>
2020-05-24 19:00:45 +0200

    API Files angelegt; Fill-Skript fuer Datenbank erstellt

---
+++ sql/fill.sql
@@ -0,0 +1,12 @@
+INSERT INTO Produkt(name, preis) VALUES 
+('Milch', 1.00), ('Naturjoghurt', 1.20), ('Quark', 0.80), ('Sahne', 0.65), ('Emmentaler', 1.30), ('Erdbeerkaese', 1.37);
+
+INSERT INTO Verkaufsstelle(typ, name, ort) VALUES
+('Haendler', 'Peters Kaeseecke', 'Ziegengasse 3'),
+('Automat', 'Automat 001', 'Milchstrasse 23'),
+('Haendler', 'Molkereiprodukte Mueller', 'Malerweg 1'),
+('Automat', 'Automat 002', 'Hauptstrasse 108'),
+('Haendler', 'Herr Fahris fahrbarer Quarkverkauf', 'mobil');
+
+INSERT INTO Lieferant(name) VALUES
+('Peter Pointer'), ('Big Dick Kadim'), ('Herr Fahri');
