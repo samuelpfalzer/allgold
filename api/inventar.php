beac36e5ed42ce432d8c8d28b1250054c245025a
Samuel Pfalzer <samuel.pfalzer@stud.hs-kempten.de>
2020-05-24 20:02:21 +0200

    Inventarabfrage implementiert

---
+++ api/inventar.php
@@ -0,0 +1,43 @@
+<?php
+
+$con = new mysqli("allgold-db", "test", "test", "allgold") or die("Datenbankverbindung fehlgeschlagen");
+
+switch ($_SERVER["REQUEST_METHOD"]) {
+    case "GET":
+        if (isset($_GET["verkaufsstelle"]) and isset($_GET["produkt"])) {
+            $verkaufsstelle = $_GET["verkaufsstelle"];
+            $produkt = $_GET["produkt"];
+            $res = $con->query("SELECT prod.name AS produkt, vorrat, bedarf FROM (SELECT produkt_id, vorrat, bedarf FROM Inventar WHERE verkaufsstelle_id = (SELECT id FROM Verkaufsstelle WHERE name='$verkaufsstelle') AND produkt_id = (SELECT id FROM Produkt WHERE name='$produkt')) AS inv, (SELECT * FROM Produkt) as prod WHERE prod.id = inv.produkt_id;");
+
+            if ($res->num_rows > 0) {
+                $inventar_element = $res->fetch_assoc();
+                http_response_code(200);
+                echo (json_encode($inventar_element));
+            } else {
+                http_response_code(404);
+            }
+        } else if (isset($_GET["verkaufsstelle"])) {
+            $verkaufsstelle = $_GET["verkaufsstelle"];
+            $res = $con->query("SELECT prod.name AS produkt, vorrat, bedarf FROM (SELECT produkt_id, vorrat, bedarf FROM Inventar WHERE verkaufsstelle_id = (SELECT id FROM Verkaufsstelle WHERE name='$verkaufsstelle')) AS inv, (SELECT * FROM Produkt) as prod WHERE prod.id = inv.produkt_id;");
+
+            if ($res->num_rows > 0) {
+                $inventar = array();
+                while ($inventar_element = $res->fetch_assoc()) {
+                    $inventar[] = $inventar_element;
+                }
+                http_response_code(200);
+                echo (json_encode($inventar));
+            } else {
+                http_response_code(404);
+            }
+        }
+
+    case "POST":
+        break;
+
+    case "PUT":
+        break;
+
+    case "DELETE":
+        break;
+}
--- sql/fill.sql
+++ sql/fill.sql
@@ -10,3 +10,16 @@ INSERT INTO Verkaufsstelle(typ, name, ort) VALUES
 
 INSERT INTO Lieferant(name) VALUES
 ('Peter Pointer'), ('Big Dick Kadim'), ('Herr Fahri');
+
+INSERT INTO Inventar(verkaufsstelle_id, produkt_id, vorrat, bedarf) VALUES 
+((SELECT id FROM Verkaufsstelle WHERE name = 'Peters Kaeseecke'), (SELECT id FROM Produkt WHERE name = 'Milch'), 0, 5),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Peters Kaeseecke'), (SELECT id FROM Produkt WHERE name = 'Naturjoghurt'), 2, 5),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Automat 001'), (SELECT id FROM Produkt WHERE name = 'Milch'), 3, 3),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Automat 001'), (SELECT id FROM Produkt WHERE name = 'Quark'), 3, 7),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Automat 001'), (SELECT id FROM Produkt WHERE name = 'Sahne'), 0, 5),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Molkereiprodukte Mueller'), (SELECT id FROM Produkt WHERE name = 'Milch'), 0, 5),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Automat 002'), (SELECT id FROM Produkt WHERE name = 'Milch'), 1,8),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Automat 002'), (SELECT id FROM Produkt WHERE name = 'Emmentaler'), 0, 5),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Herr Fahris fahrbarer Quarkverkauf'), (SELECT id FROM Produkt WHERE name = 'Emmentaler'), 4, 4),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Herr Fahris fahrbarer Quarkverkauf'), (SELECT id FROM Produkt WHERE name = 'Milch'), 0, 5),
+((SELECT id FROM Verkaufsstelle WHERE name = 'Herr Fahris fahrbarer Quarkverkauf'), (SELECT id FROM Produkt WHERE name = 'Erdbeerkaese'), 0, 5);
