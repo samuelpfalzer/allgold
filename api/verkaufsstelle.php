f4a3a04951ce85582e0c1fc5a6a29b83cf93b9d7
Samuel Pfalzer <samuelpfalzer@gmail.com>
2020-05-30 00:04:15 +0200

    API + Use Case neue Verkaufsstelle implementiert

--- api/verkaufsstelle.php
+++ api/verkaufsstelle.php
@@ -12,6 +12,22 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         echo (json_encode($verkaufsstellen));
         break;
     case "POST":
+        $payload = json_decode(file_get_contents("php://input"), true);
+        if (!(isset($payload["typ"]) and isset($payload["name"]) and isset($payload["ort"]))) {
+            http_response_code(400);
+            break;
+        } else {
+            $typ = $payload["typ"]; // TODO: validate
+            $name = $payload["name"]; // TODO: validate
+            $ort = $payload["ort"]; // TODO: validate
+            $res = $con->query("INSERT INTO Verkaufsstelle(typ, name, ort) VALUES('$typ', '$name', '$ort');");
+
+            if ($res) {
+                http_response_code(201);
+            } else {
+                http_response_code(409);
+            }
+        }
         break;
 
     case "PUT":
--- web/src/app/neue-verkaufsstelle/neue-verkaufsstelle.component.ts
+++ web/src/app/neue-verkaufsstelle/neue-verkaufsstelle.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-neue-verkaufsstelle',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./neue-verkaufsstelle.component.css']
 })
 export class NeueVerkaufsstelleComponent implements OnInit {
+  typ: string;
+  name: string;
+  ort: string;
 
-  constructor() { }
+  typen: string[] = ["Haendler", "Automat"];
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.typ = null;
+    this.name = null;
+    this.ort = null;
   }
 
+  eroeffnen() {
+    this.http.post("/api/verkaufsstelle", { name: this.name, ort: this.ort, typ: this.typ }).subscribe(e => {
+      console.log("pimmel");
+    })
+  }
 }
